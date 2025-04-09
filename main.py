a62c32f346e39f482fa29998d1314dae7dde4cd7
S. Ayberk Çolakoğlu <35194553+brk34@users.noreply.github.com>
2025-04-09 11:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_20newsgroups
 
-# Örnek veri seti
+# Sample dataset
 data = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
 X, y = data.data, data.target
 
-# TfidfVectorizer ve SVM Pipeline oluşturma
+# Create TF-IDF Vectorizer and SVM Pipeline
 vectorizer = TfidfVectorizer(stop_words='english')
 svm_model = LinearSVC()
 
-# Modeli ve vectorizer'ı birleştir
+# Combine the model and vectorizer
 model_pipeline = make_pipeline(vectorizer, svm_model)
 
-# Veriyi eğitim ve test olarak ayır
+# Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Modeli eğit
+# Train the model
 model_pipeline.fit(X_train, y_train)
 
-# Model ve Vectorizer'ı kaydet
+# Save the model and vectorizer
 with open('vectorizer2.pkl', 'wb') as f:
     pickle.dump(vectorizer, f)
 
 with open('svm2.pkl', 'wb') as f:
     pickle.dump(svm_model, f)
 
-print("Model ve vectorizer başarıyla kaydedildi.")
+print("Model and vectorizer have been successfully saved.")
